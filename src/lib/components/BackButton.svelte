6433f3328b47039d5facf6869098c8294ec6fe84
Bohns <hello@bohns.design>
2025-09-10 01:17:37 +0200

    create a back button component

---
+++ src/lib/components/BackButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let href: string | null = null
+  export let label = '← Retour'
+  function go() {
+    if (href) window.location.href = href
+    else history.back()
+  }
+</script>
+
+<button
+  class="hover:text-black/50 cursor-pointer"
+  on:click|preventDefault={go}
+>
+  {label}
+</button>
