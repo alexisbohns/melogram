6bc48b835a941328bf71e812e11cd6db34def458
Bohns <hello@bohns.design>
2025-09-10 22:22:19 +0200

    import fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,10 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+    <!-- Google Fonts: moved from CSS @import to prevent import errors -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Doto:wght@100..900&family=Seaweed+Script&family=Sora:wght@100..800&family=Tiny5&display=swap" rel="stylesheet">
 </svelte:head>
 
 {@render children?.()}
