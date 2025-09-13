fe9cc1b3894668fb755d0080e9847985c3c4c065
Bohns <hello@bohns.design>
2025-09-13 09:10:29 +0200

    git homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { goto } from '$app/navigation'
+  export let data
+  const { tracks, error } = data
+
+  function openTrack(slug: string) {
+    goto(`/tracks/${slug}`)
+  }
+</script>
+
+<section>
+  <header>
+    <h1>Morceaux</h1>
+    {#if error}<p class="text-red-600">{error}</p>{/if}
+  </header>
+
+  {#if tracks.length === 0}
+    <p>Aucun morceau.</p>
+  {:else}
+    <div class="tracks_list">
+      {#each tracks as t}
+        <a href={`/tracks/${t.slug}`} class="track_item">
+          <div class="track_item_name">{t.name}</div>
+          <div class="track_item_date">{new Date(t.created_at).toLocaleDateString()}</div>
+        </a>
+      {/each}
+    </div>
+  {/if}
+</section>
+
+<style lang="stylus">
+  .tracks_list
+      display flex
+      flex-direction column
+      gap 1rem
+
+  .track
+    &_item
+      display flex
+      flex-direction column
+
+      &:hover
+        opacity 0.5
+
+      &_name
+        font-family "Seaweed Script"
+        font-size 2rem
+
+      &_date
+        opacity 0.25
+        font-size 1rem
+</style>
