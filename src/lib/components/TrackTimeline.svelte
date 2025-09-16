43030bc0ebc362cad13ca522e11d51335c5c7164
Bohns <hello@bohns.design>
2025-09-16 07:56:28 +0200

    introduce lycrics tab

---
+++ src/lib/components/TrackTimeline.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import TrackVersionItem from '$lib/components/TrackVersionItem.svelte'
+  import type { Version } from '$lib/components/TrackVersionItem.svelte'
+  import { t } from '$lib/i18n/i18n'
+
+  type Track = { id: string; name: string }
+
+  export let track: Track
+  export let versions: Version[] = []
+</script>
+
+<section>
+  {#if versions.length === 0}
+    <p>{$t('tracks.no_versions')}</p>
+  {:else}
+    <ul class="track-versions-timeline">
+      {#each versions as v}
+        <TrackVersionItem version={v} />
+      {/each}
+    </ul>
+  {/if}
+</section>
+
+<style lang="stylus">
+.track-versions-timeline
+  list-style none
+  margin 0
+  padding 0
+  display flex
+  flex-direction column
+  gap 1rem
+</style>
+
---
+++ src/lib/components/TrackVersions.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import TrackVersionItem from '$lib/components/TrackVersionItem.svelte'
+  import type { Version } from '$lib/components/TrackVersionItem.svelte'
+  import { t } from '$lib/i18n/i18n'
+
+  export let versions: Version[] = []
+</script>
+
+<section>
+  {#if versions.length === 0}
+    <p>{$t('tracks.no_versions')}</p>
+  {:else}
+    <ul class="track-versions-list">
+      {#each versions as v}
+        <TrackVersionItem version={v} />
+      {/each}
+    </ul>
+  {/if}
+</section>
+
+<style lang="stylus">
+.track-versions-list
+  list-style none
+  margin 0
+  padding 0
+  display flex
+  flex-direction column
+  gap 1rem
+</style>
+
